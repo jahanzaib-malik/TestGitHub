86bd293e3d3c0861696d54eca0a3683c67ded3fa
jahanzaib-malik <jahanzaibmalik786@gmail.com>
2015-03-29 20:49:51 -0800

    sasasa

---
+++ typescript/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "jane user";
+document.body.innerHTML = greeter(user);
---
+++ typescript/greeter.ts
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "jane user";
+
+document.body.innerHTML = greeter(user);
